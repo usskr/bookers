1b340a9aaaee2003bae76cac0508bdd8e62c354a
usskr <rei.takagi731@gmail.com>
2021-08-06 09:13:03 +0000

    [Add] バリデーションの実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,10 +5,13 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    if book.save
+    @book = Book.new(book_params)
+    @books = Book.all
+    if @book.save
       flash[:create] = 'Book was successfully created.'
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book)
+    else
+      render :index
     end
   end
 
@@ -21,10 +24,12 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
       flash[:update] = 'Book was successfully updated.'
-      redirect_to book_path(book)
+      redirect_to book_path(@book)
+    else
+      render :edit
     end
   end
 
